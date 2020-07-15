8fdeff6b79dc290f0ab0613a4977a6cdbb72ee07
Kerem Cakirer <kcakirer@hotmail.com>
2020-07-15 11:47:05 +0300

    Extend indirect malloc() test

--- pta/extra/indirect_malloc.c
+++ pta/extra/indirect_malloc.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 
+void *g(size_t sz)
+{
+	return malloc(sz);
+}
+
 int main(void)
 {
-	void *(*f)(size_t);
-	f = malloc;
-	int *p = f(sizeof(int));
+	void *(*f)(size_t) = malloc;
+	int *a, *b;
+
+	a = f(sizeof(int));
+	b = g(sizeof(int));
+
 	return 0;
 }
