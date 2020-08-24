bcde68007a45defd742640e5318e32985a83395b
Kerem Cakirer <kcakirer@hotmail.com>
2020-08-25 01:39:44 +0300

    Simplify vla.c

--- cil/vla.c
+++ cil/vla.c
@@ -1,24 +1,6 @@
-int sum(int n, int a[n])
-{
-	int sum = 0;
-	for (int i = 0; i < n; ++i) {
-		sum += a[i];
-	}
-	return sum;
-}
-
 int main(void)
 {
-	int n = 0;
-	for (int i = 0; i < 5; ++i) {
-		++n;
-	}
-
+	int n = 4;
 	int a[n];
-	++n;
 	int i = sizeof(a);
-
-	int j = sum(sizeof(a) / sizeof(*a), a);
-
-	return 0;
 }
