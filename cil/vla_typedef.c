0f7e06324e49a0b5bba19d6b1a985ec263869234
Kerem Cakirer <kcakirer@hotmail.com>
2020-08-25 00:00:50 +0300

    Simplify VLA typedef example

--- cil/vla_typedef.c
+++ cil/vla_typedef.c
@@ -1,16 +1,9 @@
 int main(void)
 {
-	int n = 0;
-	for (int i = 0; i < 5; ++i) {
-		++n;
-	}
-
-	typedef int vla[n];
-
-	++n;
-
-	vla a;
-	int i = sizeof(a);
-
+	int n = 4;
+	typedef int vla[n]; // vla is int[4]
+	n++;
+	vla a; // n is now 5 but vla is still int[4]
+	int i = sizeof(a); // i = 4 * sizeof(int)
 	return 0;
 }
